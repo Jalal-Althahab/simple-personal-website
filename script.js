f343eff57d88c4788bc468ddfd45f5a89e29c11d
Jalal Al-Thahab <63614625+Jalal-Althahab@users.noreply.github.com>
2024-08-17 04:22:09 +0300

    The Files

---
+++ script.js
@@ -0,0 +1,58 @@
+
+let slideIndex = 0;
+let startX;
+
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 9000); // Change image every 9 seconds
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function toggleTheme() {
+    document.body.classList.toggle('dark-mode');
+    const icon = document.querySelector('.theme-toggle i');
+    if (document.body.classList.contains('dark-mode')) {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+    } else {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+    }
+}
+
+const slideshowContainer = document.getElementById('slideshow-container');
+
+slideshowContainer.addEventListener('touchstart', (e) => {
+    startX = e.touches[0].clientX;
+});
+
+    slideshowContainer.addEventListener('touchmove', (e) => {
+    if (!startX) return;
+    let moveX = e.touches[0].clientX;
+    let diffX = startX - moveX;
+
+    if (Math.abs(diffX) > 50) {
+        if (diffX > 0) {
+            plusSlides(1); // Swipe left
+        } else {
+            plusSlides(-1); // Swipe right
+        }
+        startX = null; // Reset startX after swipe
+    }
+});
+
+slideshowContainer.addEventListener('touchend', () => {
+    startX = null;
+});
