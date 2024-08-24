255b594e47825161287bc813820ba9c30bf1674c
Jalal Al-Thahab <63614625+Jalal-Althahab@users.noreply.github.com>
2024-08-25 00:04:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}    
     slides[slideIndex-1].style.display = "block";  
-    setTimeout(showSlides, 5000); // Change image every 5 seconds
+    setTimeout(showSlides, 6000); // Change image every 6 seconds
 }
 
 function plusSlides(n) {
